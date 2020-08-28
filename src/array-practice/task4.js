e476b3f815632b54170b1916e4cedd891759bda2
803142 <803142@gmail.com>
2020-08-28 10:54:39 +0300

    feat: array task 4

--- src/array-practice/task4.js
+++ src/array-practice/task4.js
@@ -7,3 +7,8 @@
  * console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
  * console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
  */
+
+export default function union(...arrays) {
+  const result = new Set(arrays.reduce((acc, val) => [...acc, ...val]));
+  return [...result];
+}
