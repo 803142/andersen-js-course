92feb98ac3b5d259b4bd25f4a6c9d0e9e7eae5f7
803142 <803142@gmail.com>
2020-09-18 13:29:05 +0300

    feat: Task3 Promise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/main.css';
 import { foo, createCb } from './asynchronous-programming/task1';
 import { parseJSON, successCb, failureCb } from './asynchronous-programming/task2';
+import { delay } from './asynchronous-programming/task3';
 
 console.log('task1');
 foo(5, createCb('cb'));
@@ -13,3 +14,6 @@ parseJSON('{"х": 10}', successCb, failureCb);
 parseJSON('{x}', successCb, failureCb);
 // 'Failure parse!'
 // SyntaxError: Unexpected token x in JSON at position 1
+
+console.log('task3');
+delay(1000).then(value => console.log(`Done with ${value}`)); // Done with 100
